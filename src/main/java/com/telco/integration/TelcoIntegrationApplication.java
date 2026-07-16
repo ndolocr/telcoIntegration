af10cec5c735b92960ddfeb74ae80cd0acb2eb12
raphaelndolo <raphael.ndolo@equitybank.co.ke>
2026-07-16 22:34:17 +0300

    Project Initiation

---
+++ src/main/java/com/telco/integration/TelcoIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.telco.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelcoIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelcoIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telco/integration/TelcoIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telco.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelcoIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
