807deb9f69f4020e49d6973f3aa5e1df52dd40eb
raphaelndolo <raphael.ndolo@equitybank.co.ke>
2026-07-17 23:37:37 +0300

    Creating TelcoProvider Service class

---
+++ src/main/java/com/telco/integration/repository/TelcoProviderRepository.java
@@ -0,0 +1,8 @@
+package com.telco.integration.repository;
+
+import com.telco.integration.entity.TelcoProvider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelcoProviderRepository extends JpaRepository<TelcoProvider, Long> {
+
+}
